ed51440dc0b28105de07c19092e4357b61834ae3
luisgabriel2603 <luisgabriel2603@gmail.com>
2023-05-03 11:13:03 -0300

    alterações para fuincionamento em versões atuais

--- src/Components/HeaderResponsive/index.js
+++ src/Components/HeaderResponsive/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import "./styles.css"
 
 
